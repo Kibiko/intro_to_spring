c66ecd0c974ebd7cedc546c1128f2c1aeec8bb48
Kibiko <kevinjunchan@gmail.com>
2023-08-03 10:51:15 +0100

    added custom request

--- src/main/java/com/bnta/hello_world/controllers/GreetingController.java
+++ src/main/java/com/bnta/hello_world/controllers/GreetingController.java
@@ -1,4 +1,14 @@
 package com.bnta.hello_world.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+
+    @GetMapping("/greeting") //sets what route triggers this method, GET
+    public String greeting(@RequestParam(defaultValue = "world") String name){ //http://localhost:8080/greeting?name=kevin
+        return "Hello "+ name;
+    }
 }
